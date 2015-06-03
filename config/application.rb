829fe0164bccd6b3f6f44bb90a10855d84f6ad9e
後藤 誉昌 <yasu1003@gmail.com>
2015-06-03 17:19:37 +0900

    Allow ACAO

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Sample
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    }
   end
 end
